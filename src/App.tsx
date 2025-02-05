6a7e884602c16c46f0c969f9abdf0b563b8f5856
Bruno Hirae <brunodeluca117@outlook.com>
2025-02-05 11:09:48 -0300

    chore: setup tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1 className='text-blue-900 text-4xl text-center'>Hello World</h1>
+    </div>
+  );
 };
 
 export default App;
