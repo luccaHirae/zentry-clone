3e3f6fc2261d07ab4bc4c051b5988119b986813d
Bruno Hirae <brunodeluca117@outlook.com>
2025-02-07 10:13:34 -0300

    feat: add contact section

---
+++ src/components/Contact.tsx
@@ -0,0 +1,62 @@
+import { Button } from './Button';
+
+export const Contact = () => {
+  return (
+    <section id='contact' className='my-20 min-h-96 w-screen px-10'>
+      <div className='relative rounded-lg bg-black py-24 text-blue-50 sm:overflow-hidden'>
+        <div className='absolute -left-20 top-0 hidden h-full w-72 overflow-hidden sm:block lg:left-20 lg:w-96'>
+          <ImageClipBox
+            src='img/contact-1.webp'
+            alt='blazing rock'
+            className='contact-clip-path-1'
+          />
+          <ImageClipBox
+            src='img/contact-2.webp'
+            alt='magic device'
+            className='contact-clip-path-2 lg:translate-y-40 translate-y-60'
+          />
+        </div>
+
+        <div className='absolute -top-40 left-20 w-60 sm:top-1/2 md:left-auto md:right-10 lg:top-20 lg:w-80'>
+          <ImageClipBox
+            src='img/swordman-partial.webp'
+            alt='blazing rock'
+            className='absolute md:scale-125'
+          />
+          <ImageClipBox
+            src='img/swordman.webp'
+            alt='magic device'
+            className='sword-man-clip-path md:scale-125'
+          />
+        </div>
+
+        <div className='flex flex-col items-center text-center'>
+          <p className='font-general text-[10px] uppercase'>Join Zentry</p>
+
+          <p className='mt-10 w-full font-zentry text-5xl leading-[0.9] md:text-[6rem]'>
+            Let's b<b>u</b>ild the <br /> new er<b>a</b> of <br /> gaming t
+            <b>o</b>gether
+          </p>
+
+          <Button className='mt-10 cursor-pointer'>Contact Us</Button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export const ImageClipBox = ({
+  src,
+  alt,
+  className,
+}: {
+  src: string;
+  alt?: string;
+  className?: string;
+}) => {
+  return (
+    <div className={className}>
+      <img src={src} alt={alt} />
+    </div>
+  );
+};
