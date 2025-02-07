8a458af5ac931a94fc6c8ff4e73285227f089a59
Bruno Hirae <brunodeluca117@outlook.com>
2025-02-07 10:25:39 -0300

    fix: card title spelling

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -22,7 +22,7 @@ export const Features = () => {
             src='videos/feature-1.mp4'
             title={
               <>
-                radi<b>n</b>t
+                radia<b>n</b>t
               </>
             }
             description='A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure.'
